221ed6f364dd294fcfd42226378492f829df9514
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 15:12:43 -0400

    Add the GetUser func to UserDatasource interface

--- internal/domain/datasource/database/user.go
+++ internal/domain/datasource/database/user.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/daniarmas/notes/internal/entity"
+	"github.com/google/uuid"
 )
 
 type UserDatasource interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 	UpdateUser(ctx context.Context, user *entity.User) (*entity.User, error)
+	GetUser(ctx context.Context, id uuid.UUID) (*entity.User, error)
 }
