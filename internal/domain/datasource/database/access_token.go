321161c553710a4fe5d91ad18a3ae010a82f76cf
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 16:07:35 -0400

    Add the AccessTokenDatabaseDs interface

---
+++ internal/domain/datasource/database/access_token.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"context"
+
+	"github.com/daniarmas/notes/internal/entity"
+	"github.com/google/uuid"
+)
+
+type AccessTokenDatabaseDs interface {
+	GetAccessToken(ctx context.Context, id uuid.UUID) (*entity.AccessToken, error)
+	CreateAccessToken(ctx context.Context, accessToken *entity.AccessToken) (*entity.AccessToken, error)
+	DeleteAccessToken(ctx context.Context, id uuid.UUID) error
+}
