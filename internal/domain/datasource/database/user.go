805470b095a108af456702c71514c8014a9a2f01
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 15:37:06 -0400

    Modify UserDatasource to UserDatabaseDs

--- internal/domain/datasource/database/user.go
+++ internal/domain/datasource/database/user.go
@@ -7,7 +7,7 @@ import (
 	"github.com/google/uuid"
 )
 
-type UserDatasource interface {
+type UserDatabaseDs interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 	UpdateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 	GetUser(ctx context.Context, id uuid.UUID) (*entity.User, error)
