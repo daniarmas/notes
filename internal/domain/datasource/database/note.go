a72801ecd374f10033ba57695165e21b01ad2057
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 15:39:12 -0400

    Fixes in the params names

--- internal/domain/datasource/database/note.go
+++ internal/domain/datasource/database/note.go
@@ -10,7 +10,7 @@ import (
 type NoteDatabaseDs interface {
 	ListNote(ctx context.Context) (*[]entity.Note, error)
 	GetNote(ctx context.Context, id uuid.UUID) (*entity.Note, error)
-	CreateNote(ctx context.Context, user *entity.Note) (*entity.Note, error)
-	UpdateNote(ctx context.Context, user *entity.Note) (*entity.Note, error)
+	CreateNote(ctx context.Context, note *entity.Note) (*entity.Note, error)
+	UpdateNote(ctx context.Context, note *entity.Note) (*entity.Note, error)
 	DeleteNote(ctx context.Context, id uuid.UUID) error
 }
