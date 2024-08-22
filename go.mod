4a29a2c3617d24ed6066782e102f6a1272419601
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 19:00:57 -0400

    Add implementation of domain hash datasource

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/daniarmas/notes
 go 1.22.4
 
 require (
-	github.com/spf13/cobra v1.8.1
 	github.com/google/uuid v1.6.0
+	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.26.0
 )
---
+++ internal/data/datasource/hash/hash.go
@@ -0,0 +1,23 @@
+package datahashds
+
+import (
+	"github.com/daniarmas/notes/internal/domain/datasource/hash"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type hashDatasource struct {
+}
+
+func NewBcryptHash() domainhashds.HashDs {
+	return &hashDatasource{}
+}
+
+func (ds hashDatasource) Hash(value string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(value), 10)
+	return string(bytes), err
+}
+
+func (ds hashDatasource) CheckHash(value, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(value))
+	return err == nil
+}
