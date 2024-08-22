71a331b9589cf50214390a2f025d2dabb08496ab
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 15:13:41 -0400

    Modify the UpdateUser function in the UserDatasource interface to update the user parameter

--- internal/domain/datasource/database/user.go
+++ internal/domain/datasource/database/user.go
@@ -9,6 +9,6 @@ import (
 
 type UserDatasource interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
-	UpdateUser(ctx context.Context, user *entity.User) (*entity.User, error)
+	UpdateUser(ctx context.Context, id uuid.UUID) (*entity.User, error)
 	GetUser(ctx context.Context, id uuid.UUID) (*entity.User, error)
 }
