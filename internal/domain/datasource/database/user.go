d97e744af677ebd99232c410968c76f6e568a477
daniarmas <daniel.armas9706@gmail.com>
2024-08-22 15:08:59 -0400

    Add User database interface

---
+++ internal/domain/datasource/database/user.go
@@ -0,0 +1,11 @@
+package database
+
+import (
+	"context"
+
+	"github.com/daniarmas/notes/internal/entity"
+)
+
+type UserDatasource interface {
+	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
+}
