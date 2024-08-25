6b022fb3c7de07c199a426a59c457494ae8e41e9
daniarmas <daniel.armas9706@gmail.com>
2024-08-25 17:51:39 -0400

    Add the healthcheck handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/daniarmas/notes/internal/config"
@@ -24,4 +25,17 @@ func addRoutes(
 	config *config.Configuration,
 ) {
 	mux.Handle("/", http.NotFoundHandler())
+	mux.HandleFunc("GET /health", handleHealthCheck)
+}
+
+// healthcheck handler
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	// HealthCheckResponse represents the structure of the health check response
+	type HealthCheckResponse struct {
+		Status string `json:"status"`
+	}
+	response := HealthCheckResponse{Status: "healthy"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
