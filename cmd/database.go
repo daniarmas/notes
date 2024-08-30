c1472f7a57da8c0979ba8419cff26ec5bc87b988
daniarmas <daniel.armas9706@gmail.com>
2024-08-29 20:18:15 -0400

    Add the refresh_tokens, access_tokens and notes tables creation

--- cmd/database.go
+++ cmd/database.go
@@ -42,13 +42,89 @@ to quickly create a Cobra application.`,
 		// Create users table if not exists
 		stmt, err = db.Prepare(`
 		CREATE TABLE IF NOT EXISTS users (
-    		id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    		id UUID DEFAULT gen_random_uuid(),
     		name STRING NOT NULL,
     		email STRING NOT NULL UNIQUE,
 			password STRING NOT NULL,
     		create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    		update_time TIMESTAMP
+    		update_time TIMESTAMP,
+			CONSTRAINT pk PRIMARY KEY (id)
 		);`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = stmt.Exec()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create refresh tokens table if not exists
+		stmt, err = db.Prepare(`
+			CREATE TABLE IF NOT EXISTS refresh_tokens (
+				id UUID DEFAULT gen_random_uuid(),
+				user_id UUID NOT NULL,
+    			create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    			update_time TIMESTAMP,
+				CONSTRAINT pk PRIMARY KEY (id),
+				CONSTRAINT fk_user
+        			FOREIGN KEY (user_id) 
+        			REFERENCES users(id)
+        			ON DELETE CASCADE
+			)
+		`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = stmt.Exec()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create access tokens table if not exists
+		stmt, err = db.Prepare(`
+			CREATE TABLE IF NOT EXISTS access_tokens (
+				id UUID DEFAULT gen_random_uuid(),
+				user_id UUID NOT NULL,
+				refresh_token_id UUID NOT NULL,
+    			create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    			update_time TIMESTAMP,
+				CONSTRAINT pk PRIMARY KEY (id),
+				CONSTRAINT fk_user
+        			FOREIGN KEY (user_id) 
+        			REFERENCES users(id)
+        			ON DELETE CASCADE,
+				CONSTRAINT fk_refresh_token
+        			FOREIGN KEY (refresh_token_id) 
+        			REFERENCES refresh_tokens(id)
+        			ON DELETE CASCADE
+			)
+		`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = stmt.Exec()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create notes table if not exists
+		stmt, err = db.Prepare(`
+			CREATE TABLE IF NOT EXISTS notes (
+				id UUID DEFAULT gen_random_uuid(),
+				user_id UUID NOT NULL,
+				title STRING,
+				content STRING,
+				background_color STRING(7),
+    			create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    			update_time TIMESTAMP,
+    			delete_time TIMESTAMP,
+				CONSTRAINT pk PRIMARY KEY (id),
+				CONSTRAINT fk_user
+        			FOREIGN KEY (user_id) 
+        			REFERENCES users(id)
+        			ON DELETE CASCADE
+			)
+		`)
 		if err != nil {
 			log.Fatal(err)
 		}
