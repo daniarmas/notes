b15d922400e907ad8d33ea75ce1d140231d3cea4
daniarmas <daniel.armas9706@gmail.com>
2024-09-22 18:09:40 -0400

    Add the GetUserByEmail implementation to the user cache datasource

--- internal/domain/user_cache_ds.go
+++ internal/domain/user_cache_ds.go
@@ -7,6 +7,7 @@ import (
 )
 
 type UserCacheDs interface {
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
 	GetUserById(ctx context.Context, id uuid.UUID) (*User, error)
 	CreateUser(ctx context.Context, user *User) error
 	UpdateUser(ctx context.Context, user *User) error
