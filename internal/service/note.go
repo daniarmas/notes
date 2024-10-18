69f615c8a1513c2aac8156ea31c49c6a6f38493a
daniarmas <daniel.armas9706@gmail.com>
2024-10-18 19:00:33 -0400

    Add the CreateNote service implementation

--- internal/service/note.go
+++ internal/service/note.go
@@ -7,11 +7,31 @@ import (
 )
 
 type CreateNoteResponse struct {
-	Note domain.Note `json:"note"`
+	Note *domain.Note `json:"note"`
 }
 
 type NoteService interface {
 	CreateNote(ctx context.Context, title string, content string) (*CreateNoteResponse, error)
 }
 
+type noteService struct {
+	NoteRepository domain.NoteRepository
+}
 
+func NewNoteService(noteRepository domain.NoteRepository) NoteService {
+	return &noteService{
+		NoteRepository: noteRepository,
+	}
+}
+
+func (s *noteService) CreateNote(ctx context.Context, title string, content string) (*CreateNoteResponse, error) {
+	note := &domain.Note{
+		Title:   title,
+		Content: content,
+	}
+	note, err := s.NoteRepository.CreateNote(ctx, note)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateNoteResponse{Note: note}, nil
+}
