4e4ff8e0ec6c234cf8c480db47ce922ffc759e28
daniarmas <daniel.armas9706@gmail.com>
2025-01-14 12:44:17 -0500

    Add the CreateJob func implementation

--- internal/ck8s/ck8s.go
+++ internal/ck8s/ck8s.go
@@ -2,8 +2,12 @@ package ck8s
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/daniarmas/notes/internal/clog"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -33,5 +37,39 @@ func NewClient() error {
 		Clientset: clientset,
 	}
 
+	return nil
+}
+
+// CreateJob creates a job in the k8s cluster
+func CreateJob(ctx context.Context, jobName, namespace, imageName string, args []string) error {
+	// Create a job spec
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: jobName,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					Containers: []corev1.Container{
+						{
+							Name:  jobName,
+							Image: imageName,
+							Args:  args,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Create the job
+	job, err := ck8s.Clientset.BatchV1().Jobs(namespace).Create(ctx, job, metav1.CreateOptions{})
+	if err != nil {
+		clog.Error(context.Background(), "error creating job", err)
+		return err
+	}
+
+	clog.Info(context.Background(), fmt.Sprintf("job created successful: %s", job.Name), nil)
 	return nil
 }
